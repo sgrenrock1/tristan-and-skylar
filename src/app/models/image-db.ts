c2f01b5cf398b6cb9c598a42572c19de0f17379c
Steven Grenrock <steven.grenrock@panasonic.aero>
2019-09-04 10:50:16 -0700

    changed images

--- src/app/models/image-db.ts
+++ src/app/models/image-db.ts
@@ -5,14 +5,16 @@ export class ImageDB {
 
     constructor() {
         this.mainGallery = [
-            {url: "assets/img/2015-02-11 col.jpg", index: 0, caption: "...", redirect: "https://www.google.com" },
-            {url: "assets/img/all 3.jpg", index: 1, caption: "...", redirect: "https://www.google.com" },
-            {url: "assets/img/mckenzie.jpg", index: 0, caption: "...", redirect: "https://www.google.com" },
-            {url: "assets/img/skylar.jpg", index: 0, caption: "...", redirect: "https://www.google.com" },
-            {url: "assets/img/tns.jpg", index: 0, caption: "...", redirect: "https://www.google.com" },
-            {url: "assets/img/tristan.jpg", index: 0, caption: "...", redirect: "https://www.google.com" },
-            {url: "assets/img/tristan-sexy.jpg", index: 0, caption: "...", redirect: "https://www.google.com" },
-            {url: "assets/img/two.jpg", index: 0, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/00.jpeg", index: 0, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/01.jpeg", index: 1, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/02.jpeg", index: 0, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/03.jpeg", index: 0, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/04.jpeg", index: 0, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/05.jpeg", index: 0, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/06.jpeg", index: 0, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/07.jpeg", index: 0, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/08.jpeg", index: 0, caption: "...", redirect: "https://www.google.com" },
+            {url: "assets/arch/09.jpeg", index: 0, caption: "...", redirect: "https://www.google.com" }
         ];
     }
 }
